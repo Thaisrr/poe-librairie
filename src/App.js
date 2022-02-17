5c014df1f42664eb1c34ce6f1d744b59a4768c0c
Thaïs <thais.r@live.fr>
2022-02-17 11:42:31 +0100

    :sparkles: connexion : button

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import {useState} from "react";
 
 function App() {
   const [isConnected, setConnected] = useState(false);
+    // useState pour le nom
 
   return (
     <div className="App">
       <Title/>
+        {/* Ici, afficher le nom
+        + passer les infos en props à Connexion
+        */}
       <Connexion isConnected={isConnected} setConnected={setConnected}/>
 
         { (isConnected)? <Librairie/> : <p>Veuillez vous connecter</p> }
--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,9 +1,16 @@
 const Connexion = ({ isConnected, setConnected }) => {
+    // Récupérer les props de nom
 
-    const login = () => setConnected( !isConnected );
+    const login = () => {
+        setConnected( !isConnected );
+        // Modifier la fonction login pour récupérer la valeur de l'input
+        // On peut récupérer l'input par un selecteur, ou via un form
+        // On modifie le hook du nom
+    }
 
     return (
         <>
+            {(!isConnected) && <input/> }
             <button onClick={login}>
                 { (isConnected)? 'Déconnexion' : 'Connexion'}
             </button>
