9612f9096db25d54cc294c302f1fe9c137c85703
Thaïs <thais.r@live.fr>
2022-02-18 11:45:09 +0100

    :sparkles: connexion API ( json-server ) :memo: json-server --w src/tools/db/db.json --port 4600

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
-const Card = ({title, txt, action, img}) => (
+import '../style/Card.css'
+
+const Card = ({title, txt, action, img, status}) => (
     <div className="card">
         <div className="card-header">
             <img src={img} alt={title}/>
         </div>
         <div className="card-body">
-            <h3>{title}</h3>
+            <h3 style={{color: (status)? 'green' : 'red'}}>{title}</h3>
             <p>{txt}</p>
             {(action) && <button onClick={() => action()}>Clic</button>}
         </div>
-
     </div>
 )
+export default Card;
--- src/components/Librairie.js
+++ src/components/Librairie.js
@@ -1,42 +1,36 @@
 import '../style/Librairie.css';
-
+import Card from "./Card.js";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Librairie = () => {
+    const api = "http://localhost:4600/shows";
+    const [shows, setShows] = useState([]);
 
-
-    const shows = [
-        {id: 1, title: "The Walking Dead", isWatched: true, image: 'Images/wd.jpg'},
-        {id: 2, title: "The Mandalorian", isWatched: true, image: "Images/grogu.jpg"},
-        {id: 3, title: "Boba Fett", isWatched: false, image: "Images/grogu.jpg"}
-    ]
-    
-    const getColor = (status) => {
-       
-        return (status)? 'green' : 'red';
+    function getShows() {
+        axios.get(api).then( ({data}) => {
+            setShows(data);
+        })
+        /* ( pour cette version, il faut faire une async function
+        const {data} = await axios.get(api);
+        setShows(data);
+         */
     }
 
+    useEffect(() => {
+        getShows();
+    }, []);
 
     return (
     <>
-        <h2>Mes livres</h2>
-
-        {(!shows || shows.length === 0)? 
+        <h2>Mes Séries</h2>
+        {(!shows || shows.length === 0)?
             <p>Votre librairie est vide :( </p>
             :
             <div className="grid">
-            {
-                shows.map(sh => (
-                    <div className="card" key={sh.id}>
-                        <img src={sh.image} alt={sh.title}/>
-                        <span style={{color: getColor(sh.isWatched)}}>
-                            {sh.title}
-                        </span>
-                    </div>
-                ))
-            }
+                {shows.map(sh => <Card key={sh.id} title={sh.title} img={sh.image} status={sh.isWatched}/>)}
             </div>
         }
-
     </>
     )
 };
